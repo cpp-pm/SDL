d726bb3ddb4c048af61c7faa35b8b69cc52c72cd
Dmitry Rodin <rodin.dmitry@gmail.com>
2021-06-29 13:13:31 +0300

    Add SDL2::SDL2 ALIAS target for static build

--- SDL2Config.cmake
+++ SDL2Config.cmake
@@ -1,1 +1,10 @@
 include("${CMAKE_CURRENT_LIST_DIR}/SDL2Targets.cmake")
+
+if(NOT TARGET SDL2::SDL2)
+  set_target_properties(
+    SDL2::SDL2-static
+    PROPERTIES
+    IMPORTED_GLOBAL True
+  )
+  add_library(SDL2::SDL2 ALIAS SDL2::SDL2-static)
+endif()
